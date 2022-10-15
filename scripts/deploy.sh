0e7ae389005c071e93f7764f67d8a0c4eccb67dc
Jeff Palm <jeff@jeffpalm.com>
2022-10-15 09:55:24 -0400

    update Sat Oct 15 09:55:24 EDT 2022

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,18 @@
 set -e
 
 SCRIPTS=$(dirname $0)
+DIRS="../lyft ../resy ../opentable ../opensecrets"
 
 DEPLOY_DIR=../apidumpsterfire-deploy
 mkdir -p $DEPLOY_DIR
 mkdir -p $DEPLOY_DIR/scripts
 cp $SCRIPTS/deploy.sh.txt $DEPLOY_DIR/scripts/deploy-to-keroku.sh
+
+for dir in $DIRS; do
+  pushd $dir
+  go generate ./...
+  popd
+done
 
 pushd $DEPLOY_DIR
 scripts/deploy-to-keroku.sh
